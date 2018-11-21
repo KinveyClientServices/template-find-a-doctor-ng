50b8d3e56b402753d123581932b9afb9fb35743a
Ronald Heiney <ronaldheiney@Ronalds-MacBook-Pro.local>
2018-11-21 15:17:58 -0700

    Added type Network t0 data store

--- src/app/shared/services/plan.service.ts
+++ src/app/shared/services/plan.service.ts
@@ -5,13 +5,12 @@ import { Plan } from "../models/plan.model";
 @Injectable()
 export class PlanService {
     private _plans: Array<Plan>;
-
-    private _planStore = Kinvey.DataStore.collection<Plan>("RapidHealthPlans");
+    private _planStore = Kinvey.DataStore.collection<Plan>("RapidHealthPlans", Kinvey.DataStoreType.Network);
     private _plansPromise: Promise<any>;
 
     getPlanById(id: string): Promise<any> {
         const query = new Kinvey.Query();
-        query.equalTo("plan_id",id);
+        query.equalTo("plan_id", id);
         return this._planStore.find(query).toPromise()
             .then(data => {
                 return <Plan>data[0];
